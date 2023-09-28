f89ccbd3ad20a62a6ffdb4f6f577907b2f4d12f8
Roman_Batrakov <kyct69@yandex.ru>
2023-09-28 21:39:33 +0300

    Решил первую задачу

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -5,3 +5,34 @@ m = 3, n = 4.
 5 -2 33 -2
 77 3 8 1
 */
+// Метод создания двумерного массива с рандомными числами
+int[,] ArrayGenerator (int m, int n, int min, int max)
+{
+int[,] result = new int [m, n];
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        result[i, j] = rand.Next(min, max);
+    }   
+}
+    return result;
+}
+
+
+void PrintDoubleArray (int[,] DoubleArray)
+{
+    for (int i = 0; i < DoubleArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < DoubleArray.GetLength(1); j++)
+        {
+            Console.Write($"{DoubleArray[i, j]} ");
+        }
+        Console.WriteLine ();   
+    }
+}
+
+
+int [,] DoubleArray = ArrayGenerator(3, 4, -99, 99);
+PrintDoubleArray(DoubleArray);
