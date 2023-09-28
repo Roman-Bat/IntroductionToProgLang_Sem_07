1b2deb0898296727595bbb93c82be2516f494c8f
Roman_Batrakov <kyct69@yandex.ru>
2023-09-28 22:49:03 +0300

    Решил третью задачу

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -11,3 +11,53 @@
 5 81 2 9
 8 4 2 4
 */
+
+// Метод создания двумерного массива с рандомными числами
+int[,] ArrayGenerator (int m, int n, int min, int max)
+{
+int[,] result = new int [m, n];
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        result[i, j] = rand.Next(min, max);
+    }   
+}
+    return result;
+}
+
+int[,] SqrtArrayPoint(int[,] doubleArray)
+{
+    int[,] result = new int [doubleArray.GetLength(0), doubleArray.GetLength(1)];
+    for (int i = 0; i < doubleArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doubleArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0
+                && j % 2 == 0
+            ) {result[i, j] = doubleArray[i, j] * doubleArray[i, j];}
+            else {result[i, j] = doubleArray[i, j];}
+        }   
+    }
+        return result;
+}
+
+// Метод вывода двумерного массива
+void PrintDoubleArray (int[,] doubleArray)
+{
+    for (int i = 0; i < doubleArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doubleArray.GetLength(1); j++)
+        {
+            Console.Write($"{doubleArray[i, j]} ");
+        }
+        Console.WriteLine ();   
+    }
+}
+
+int [,] doubleArray = ArrayGenerator(4, 4, -9, 10);
+PrintDoubleArray(doubleArray);
+Console.WriteLine (); 
+int[,] sqrtArray = SqrtArrayPoint(doubleArray);
+PrintDoubleArray(sqrtArray);
