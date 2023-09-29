5d11e00af00e712e9fb94c9cec4a954b95b4ee5b
Roman-Bat <kyct69@yandex.ru>
2023-09-29 18:07:11 +0300

    Решил третью Домшнюю работу

--- HomeTask_003/Program.cs
+++ HomeTask_003/Program.cs
@@ -9,3 +9,60 @@
 Среднее арифметическое
 каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+// Метод создания двумерного массива с рандомными числами
+int[,] ArrayGenerator (int m, int n, int min, int max)
+{
+int[,] result = new int [m, n];
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        result[i, j] = rand.Next(min, max);
+    }   
+}
+    return result;
+}
+
+// Метод вывода двумерного массива
+void PrintDoubleArray (int[,] doubleArray)
+{
+    for (int i = 0; i < doubleArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < doubleArray.GetLength(1); j++)
+        {
+            Console.Write($"{doubleArray[i, j]} ");
+        }
+        Console.WriteLine ();   
+    }
+}
+// Метод поиска элемента по числу от пользователя 
+void SumOfColums(int[,] randArray)
+{
+    
+    for (int i = 0; i < randArray.GetLength(1); i++)
+    {
+        double result = 0;
+        Console.WriteLine($"Считаем элементы столбца {i+1} :");
+    for (int j = 0; j < randArray.GetLength(0); j++)
+        {
+          result += randArray[j, i];
+          if (j == 0) {Console.Write("(");}
+          Console.Write($"{randArray[j, i]}");
+          if (j < randArray.GetLength(0) - 1) {Console.Write(" + ");}
+          if (j == randArray.GetLength(0) - 1) {Console.Write(")");}
+        } 
+        result = Math.Round(result / randArray.GetLength(0), 2);
+        Console.WriteLine($" / {randArray.GetLength(0)} = {result} ");
+    }
+}
+
+
+Console.WriteLine();
+int [,] doubleArray = ArrayGenerator(2, 2, 0, 10);
+Console.WriteLine("Создаем рандомный массив");
+PrintDoubleArray(doubleArray);
+Console.WriteLine();
+SumOfColums(doubleArray);
+
